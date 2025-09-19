1af8655d166e1a6eded49efc223c1fb2615beded
Mattias Jons <mattias.jons1@gmail.com>
2025-09-19 09:22:01 +0200

    testing tools

--- main.py
+++ main.py
@@ -2,15 +2,82 @@ import requests
 import os
 
 # Public
-OLLAMA_SERVER_URL = "http://" + os.getenv('LLM_SERVER') + "/api/generate"
+OLLAMA_SERVER_URL = "http://" + os.getenv('LLM_SERVER') + "/api/chat"
+
+
+
+
+def get_current_weather(params: list) -> str:
+    # Dummy implementation for illustration
+    return "The current weather in {} is 72 degrees {}".format(params[0], params[1])
+
+
+tools = {
+    "get_current_weather": get_current_weather,
+}
+
+tool_calls = [
+    {
+        "name": "get_current_weather",
+        "description": "Get the current weather in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The location to get the weather for, e.g. San Francisco, CA"
+                },
+                "format": {
+                    "type": "string",
+                    "description": "The format to return the weather in, e.g. 'celsius' or 'fahrenheit'",
+                    "enum": ["celsius", "fahrenheit"]
+            }
+            },
+            "required": ["location", "format"]
+        }
+    },
+]
 
 # Available models: llama3.2, qwen3:30b, gpt-oss:20b, deepseek-r1:8b, deepseek-r1:32b, mistral
 payload = {
-    "model": "mistral",  # Change to your desired model
-    "prompt": "Give me a recipe for a chocolate cake.",
-    "stream": False
+  "model": "llama3.2",
+  "messages": [
+    {
+      "role": "user",
+      "content": "What is the weather today in Paris?"
+    }
+  ],
+  "stream": False,
+  "tools": [
+    {
+      "type": "function",
+      "function": {
+        "name": "get_current_weather",
+        "description": "Get the current weather for a location",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "location": {
+              "type": "string",
+              "description": "The location to get the weather for, e.g. San Francisco, CA"
+            },
+            "format": {
+              "type": "string",
+              "description": "The format to return the weather in, e.g. 'celsius' or 'fahrenheit'",
+              "enum": ["celsius", "fahrenheit"]
+            }
+          },
+          "required": ["location", "format"]
+        }
+      }
+    }
+  ]
 }
 
-response = requests.post(OLLAMA_SERVER_URL, json=payload)
-print(response.json())
-print(response.json()['response'])
+def main():
+    response = requests.post(OLLAMA_SERVER_URL, json=payload)
+    print(response.json())
+    # print(response.json()['response'])
+
+if __name__ == "__main__":
+    main()
